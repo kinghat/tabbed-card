38f2ea6634964d4529571aa2f8b0f2fae638d7d3
kinghat <kinghat@protonmail.com>
2022-08-26 01:43:54 +0000

    commit all

--- src/tabbed-card.ts
+++ src/tabbed-card.ts
@@ -10,6 +10,7 @@ import {
 import { repeat } from "lit/directives/repeat.js";
 import {
   getLovelace,
+  hasConfigOrEntityChanged,
   HomeAssistant,
   LovelaceCard,
   LovelaceCardConfig,
@@ -49,24 +50,47 @@ export class TabbedCard extends LitElement {
 
   @state() private _config!: LovelaceCardConfig;
   // @state() private _cards!: ILovelaceCard[];
-  @state() private _cards!: ILovelaceCard[];
-  @state() private _initialized!: boolean;
+  @state() private _cards: ILovelaceCard[] = [];
+  // @state() private _isInitialized = false;
 
   static styles = [unsafeCSS(styles)];
 
-  private initialize() {
-    if (!this.hass && !this._config && !this._helpers) return;
-    this._initialized = true;
-  }
+  // private initialize() {
+  //   console.log("initialize:");
+  //   // if (!this.hass || !this._config || !this._helpers || !this._cards) {
+  //   //   // this.requestUpdate();
 
-  // protected shouldUpdate() {
-  //   if (!this._initialized) this.initialize();
+  //   //   return;
+  //   // }
 
-  //   return true;
+  //   if (!this._config) {
+  //     return;
+  //   }
+  //   if (!this.hass) {
+  //     // this.hass = this.hass;
+  //     return;
+  //   }
+  //   if (!this._helpers) {
+  //     this.loadCardHelpers();
+  //     // this.requestUpdate();
+
+  //     return;
+  //   }
+  //   if (!this._cards.length) {
+  //     this._createCards(this._config);
+  //     console.log("initialize: _createCards called");
+
+  //     return;
+  //   }
+
+  //   // this.requestUpdate();
+
+  //   this._isInitialized = true;
   // }
 
   protected async loadCardHelpers() {
     this._helpers = await (window as any).loadCardHelpers();
+    // this.requestUpdate();
   }
 
   static async getConfigElement(): Promise<LovelaceCardEditor> {
@@ -79,14 +103,18 @@ export class TabbedCard extends LitElement {
 
   // https://lit.dev/docs/components/properties/#accessors-custom
   public setConfig(config: LovelaceCardConfig) {
+    console.log("setConfig: this: ", { this: this });
+
     if (!config) {
       throw new Error("No configuration.");
     }
 
     this._config = {
-      name: "TabbedCard",
+      // name: "TabbedCard",
       ...config,
     };
+
+    // this.initialize();
 
     this.loadCardHelpers();
   }
@@ -95,9 +123,100 @@ export class TabbedCard extends LitElement {
   @query(".content--active") private activeContentElement!: HTMLElement;
   @queryAll(".content") private contentElements!: NodeListOf<HTMLElement>;
 
-  firstUpdated() {}
+  protected shouldUpdate(
+    _changedProperties: PropertyValueMap<any> | Map<PropertyKey, unknown>,
+  ): boolean {
+    console.log("shouldUpdate: _changedProperties: ", _changedProperties);
 
-  updated() {
+    // if (!this._isInitialized) {
+    //   this.initialize();
+    //   // this.requestUpdate();
+    //   return false;
+    // }
+
+    // return hasConfigOrEntityChanged(this, _changedProperties, false);
+
+    // if (!this.hass || !this._helpers || this._config) {
+    //   console.log("shouldUpdate: this.initialized: ", this._initialized);
+
+    //   this.initialize();
+    //   // this.requestUpdate();
+
+    //   return false;
+    // }
+    // if (_changedProperties.has("_helpers")) this._createCards(this._config);
+
+    // if (this._helpers) {
+
+    // this.loadCardHelpers();
+    // this.requestUpdate();
+    // return false;
+    // }
+
+    return true;
+  }
+
+  protected willUpdate(
+    _changedProperties: PropertyValueMap<any> | Map<PropertyKey, unknown>,
+  ): void {
+    console.log("willUpdate: _changedProperties: ", _changedProperties);
+    // if (this._initialized) this._createCards(this._config);
+    if (_changedProperties.has("_helpers") && !this._cards.length) {
+      // if (!this._cards.length && this._helpers) {
+      // if (!this._config && !this._helpers && !this.hass) {
+      //   console.log("willUpdate: hass: ", this.hass);
+      this._createCards(this._config);
+      // this.requestUpdate();
+    }
+    //   // console.log("willUpdate: _cards: ", this._cards);
+    // }
+    // if (this.hass && this._config && this._helpers) {
+    // if (_changedProperties.has("_cards") && !this._cards && this.hass) {
+
+    // if (this._isInitialized && !this._cards.length) {
+    //   console.log("willUpdate: _createCards called");
+    //   console.log("willUpdate: _isInitialized: ", this._isInitialized);
+    //   this._createCards(this._config);
+
+    //   // console.log("willUpdate: _cards: ", this._cards);
+    // }
+
+    // if (_changedProperties.has("_isInitialized") && !this._isInitialized)
+    //   this.initialize();
+    if (_changedProperties.has("_helpers")) this._createCards(this._config);
+
+    if (
+      _changedProperties.has("hass") &&
+      // this._isInitialized &&
+      this._cards.length
+    ) {
+      // this._cards.map((card) => (card.hass = this.hass));
+      this._cards.forEach((card) => (card.hass = this.hass));
+      // console.log("willUpdate: _cards: ", this._cards);
+      // this.requestUpdate();
+    }
+  }
+
+  firstUpdated(
+    _changedProperties: PropertyValueMap<any> | Map<PropertyKey, unknown>,
+  ): void {
+    console.log("firstUpdated: _changedProperties: ", _changedProperties);
+    // console.log("firstUpdated: _isInitialized: ", this._isInitialized);
+
+    // if (this._isInitialized && this._cards.length) this.requestUpdate();
+    // if (!this._isInitialized) this.initialize();
+  }
+
+  updated(
+    _changedProperties: PropertyValueMap<any> | Map<PropertyKey, unknown>,
+  ): void {
+    console.log("updated: _changedPropterties: ", _changedProperties);
+    // super.updated(_changedProperties);
+
+    // if (!this._isInitialized) {
+    //   this.initialize();
+    //   // this.requestUpdate();
+    // }
     if (this.mwcTabBar && this.contentElements.length) {
       this.mwcTabBar.addEventListener(
         "MDCTabBar:activated",
@@ -111,6 +230,10 @@ export class TabbedCard extends LitElement {
         },
       );
     }
+    // if (_changedProperties.has("hass") && this.hass && this._cards) {
+    //   this._cards.map((card) => (card.hass = this.hass));
+    // console.log("willUpdate: _cards: ", this._cards);
+    // }
   }
 
   async _createCards(config: LovelaceCardConfig) {
@@ -119,26 +242,50 @@ export class TabbedCard extends LitElement {
         const cardElement = await this._helpers.createCardElement(cardConfig);
 
         cardElement.hass = this.hass;
+        // console.log("_createCards: this.hass: ", this.hass);
+        // console.log(
+        //   "_createCards: cardElement.isUpdatePending: ",
+        //   cardElement.isUpdatePending,
+        // );
+        // console.log(
+        //   "_createCards: cardElement.hasUpdated: ",
+        //   cardElement.hasUpdated,
+        // );
+        // console.log("_createCards: cardElement.hass: ", cardElement.hass);
 
+        cardElement.addEventListener(
+          "ll-rebuild",
+          (ev) => {
+            ev.stopPropagation();
+            this._rebuildCard(cardElement, config);
+          },
+          { once: true },
+        );
+
+        await cardElement.updateComplete;
+        // console.log(
+        //   "_createCards: cardElement.updateComplete: ",
+        //   await cardElement.updateComplete,
+        // );
         return cardElement;
       }),
     );
 
-    this._cards = cardElements;
     console.log("_createCards: cardElements: ", cardElements);
+    this._cards = cardElements;
+    // cardElements.map((cardElement) => console.log(cardElement.hasUpdated));
+
     // console.log("createCards: hass: ", this.hass);
+    // this.initialize();
+    // this.requestUpdate();
   }
 
-  protected willUpdate(
-    _changedProperties: PropertyValueMap<any> | Map<PropertyKey, unknown>,
-  ): void {
-    // console.log("willUpdate: _changedProperties: ", _changedProperties);
-    if (_changedProperties.has("_helpers")) this._createCards(this._config);
+  async _rebuildCard(cardElement, cardConfig) {
+    const newCardElement = await this._helpers.createCardElement(cardConfig);
 
-    if (_changedProperties.has("hass") && this.hass && this._cards) {
-      this._cards.map((card) => (card.hass = this.hass));
-      console.log("willUpdate: _cards: ", this._cards);
-    }
+    cardElement.replaceWith(newCardElement);
+
+    this._cards.splice(this._cards.indexOf(cardElement), 1, newCardElement);
   }
 
   protected getTabLabel({ _config }: ILovelaceCard) {
@@ -150,8 +297,19 @@ export class TabbedCard extends LitElement {
   }
 
   render() {
-    if (!this.hass || !this._helpers || !this._config || !this._cards)
+    // console.log("render: _isInitialized: ", this._isInitialized);
+
+    // if (!this._isInitialized || !this._cards.length) {
+    //   this.initialize();
+    //   // this.requestUpdate();
+    //   // throw new Error("No card configuration.");
+    //   return html`Uninitialized`;
+    // }
+    if (!this.hass || !this._config || !this._helpers) {
+      // if (!this.hass || !this._helpers || !this._config || !this._cards)
+      // this.requestUpdate();
       return html``;
+    }
 
     return html`
       <mwc-tab-bar>
