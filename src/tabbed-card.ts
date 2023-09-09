32c0ee398b43b4081629881f1191edb2b9743f64
kinghat <kinghat@protonmail.com>
2023-09-09 01:09:11 +0000

    fix(tabs): ensure tab component is loaded
    ha frontend stopped loading mwc-tab-bar via whatever component so it needs to be loaded manually

--- src/tabbed-card.ts
+++ src/tabbed-card.ts
@@ -61,6 +61,8 @@ export class TabbedCard extends LitElement {
 
   private async loadCardHelpers() {
     this._helpers = await (window as any).loadCardHelpers();
+
+    if (!customElements.get("mwc-tab-bar")) this._helpers.importMoreInfoControl("weather")
   }
 
   static async getConfigElement(): Promise<LovelaceCardEditor> {
